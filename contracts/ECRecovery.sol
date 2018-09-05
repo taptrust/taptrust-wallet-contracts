15750399bdd2141c619aec6cadc12bca8fbb0e63
Aleksandar Djordjevic <adjordjevic@firstbeatmedia.com>
2018-09-05 02:12:17 +0200

    Adding new library for ECRecovery functions

--- contracts/ECRecovery.sol
+++ contracts/ECRecovery.sol
@@ -11,11 +11,9 @@ library ECRecovery {
    * @dev Recover signer address from a message by using their signature.
    * @param _hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
    * @param _sig bytes signature, the signature is generated using web3.eth.sign().
+   * @return address Recovered signed address
    */
-  function recover(bytes32 _hash, bytes _sig)
-  internal
-  pure
-  returns (address)
+  function recover(bytes32 _hash, bytes _sig) internal pure returns (address)
   {
     bytes32 r;
     bytes32 s;
@@ -24,10 +22,11 @@ library ECRecovery {
     if (_sig.length != 65) {
       return (address(0));
     }
-    // Divide the signature in r, s and v variables
-    // ecrecover takes the signature parameters, and the only way to get them
+
+    // Divide the signature in r, s and v variables.
+    // ecrecover takes the signature parameters, and the only way to get them.
     // currently is to use assembly.
-    // solium-disable-next-line security/no-inline-assembly
+    // solium-disable-next-line security/no-inline-assembly.
     assembly {
       r := mload(add(_sig, 32))
       s := mload(add(_sig, 64))
@@ -47,14 +46,12 @@ library ECRecovery {
   }
 
   /**
-   * toEthSignedMessageHash.
-   * @dev prefix a bytes32 value with "\x19Ethereum Signed Message:"
-   * and hash the result,
+   * @dev toEthSignedMessageHash, prefix a bytes32 value with "\x19Ethereum Signed Message:"
+   * and hash the result.
+   * @param _hash bytes32 Hashed message which needs to be eth singed.
+   * @return butes32 Encoded message.
    */
-  function toEthSignedMessageHash(bytes32 _hash)
-  internal
-  pure
-  returns (bytes32)
+  function toEthSignedMessageHash(bytes32 _hash) internal pure returns (bytes32)
   {
     // 32 is the length in bytes of hash,
     // enforced by the type signature above
