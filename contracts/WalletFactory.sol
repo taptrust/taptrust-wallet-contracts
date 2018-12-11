afcf0c5a89048e39c21e90f2e2d7a3371a204bed
Chance Santana-Wees <figs999@gmail.com>
2018-12-10 17:36:33 -0800

    Overhaul of ProxyWallet contracts
    Simplied ProxyWallet.sol
    Added WalletFactory.sol
    Removed other contracts which are now incompatible with simplified
    wallet contract. Functionality of these deleted contracts can be added
    back in at a later date.

--- contracts/WalletFactory.sol
+++ contracts/WalletFactory.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.4.24;
 import './ProxyWallet.sol';
 
 contract WalletFactory {
-	function createNewWallet(bytes publicKey) public returns (address) {
-		return address(new ProxyWallet(publicKey));
+	
+	event WalletCreated(address indexed wallet);
+
+	function createNewWallet(bytes publicKey) public {
+		emit WalletCreated(address(new ProxyWallet(publicKey)));
 	}
 }
